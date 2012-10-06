fc49b369b82d19f67e1668db57531f8ee2cddfa0
Paul Stadig <paul@stadig.name>
2012-10-06 00:02:06 -0400

    Add some usage notes.

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
 (defproject name.stadig/polyfn "1.0.0"
-  :description "Open, single dispatch functions."
+  :description "Open, single dispatch polymorphism."
   :dependencies [[org.clojure/clojure "1.4.0"]])
