f5f15c5e9269e467187db2b6d583bd57ba019d04
Paul Stadig <paul@stadig.name>
2012-10-07 13:07:11 -0400

    Release 1.0.2.

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
-(defproject name.stadig/polyfn "1.0.1"
+(defproject name.stadig/polyfn "1.0.2"
   :description "Open, single dispatch polymorphism."
   :dependencies [[org.clojure/clojure "1.4.0"]])
