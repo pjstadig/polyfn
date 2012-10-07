adc5b481ea7b13bb7147dc75ae00878043ce8144
Paul Stadig <paul@stadig.name>
2012-10-06 20:14:36 -0400

    Performance improvements.
    Uses __methodImplCache like protocol fns.  Performance is now essentially the
    same as protocol fns.

--- src/name/stadig/polyfn.clj
+++ src/name/stadig/polyfn.clj
@@ -1,4 +1,4 @@
-;;;; Copyright (c) Paul Stadig. All rights reserved.
+;;;; Copyright (c) Rich Hickey and Paul Stadig. All rights reserved.
 ;;;;
 ;;;; The use and distribution terms for this software are covered by the Eclipse
 ;;;; Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php) which
@@ -7,32 +7,96 @@
 ;;;; terms of this license.  You must not remove this notice, or any other, from
 ;;;; this software.
 (ns name.stadig.polyfn
-  (:require [name.stadig.polyfn.lookup :refer [find-polyfn-impl]])
-  (:import (clojure.lang IPersistentCollection)))
+  (:import (clojure.lang AFunction MethodImplCache)))
+
+(defn- shift-mask [shift mask x]
+  (-> x (bit-shift-right shift) (bit-and mask)))
+
+(def ^:private max-mask-bits 13)
+(def ^:private max-switch-table-size (bit-shift-left 1 max-mask-bits))
+
+(defn- maybe-min-hash
+  "takes a collection of hashes and returns [shift mask] or nil if none found"
+  [hashes]
+  (first
+   (filter (fn [[s m]]
+             (apply distinct? (map #(shift-mask s m %) hashes)))
+           (for [mask (map #(dec (bit-shift-left 1 %))
+                           (range 1 (inc max-mask-bits)))
+                 shift (range 0 31)]
+             [shift mask]))))
+
+(defn- expand-method-impl-cache [^clojure.lang.MethodImplCache cache c f]
+  (if (.map cache)
+    (let [cs (assoc (.map cache) c (clojure.lang.MethodImplCache$Entry. c f))]
+      (MethodImplCache. (.protocol cache) (.methodk cache) cs))
+    (let [cs (into {} (remove (fn [[c e]] (nil? e))
+                              (map vec (partition 2 (.table cache)))))
+          cs (assoc cs c (clojure.lang.MethodImplCache$Entry. c f))]
+      (if-let [[shift mask] (maybe-min-hash (map hash (keys cs)))]
+        (let [table (make-array Object (* 2 (inc mask)))
+              table (reduce (fn [^objects t [c e]]
+                              (let [i (* 2 (int (shift-mask shift mask
+                                                            (hash c))))]
+                                (aset t i c)
+                                (aset t (inc i) e)
+                                t))
+                            table cs)]
+          (MethodImplCache. (.protocol cache) (.methodk cache)
+                            shift mask table))
+        (MethodImplCache. (.protocol cache) (.methodk cache)
+                          cs)))))
+
+(defn- super-chain [^Class c]
+  (when c
+    (cons c (super-chain (.getSuperclass c)))))
+
+(defn- pref
+  ([] nil)
+  ([a] a)
+  ([^Class a ^Class b]
+     (if (.isAssignableFrom a b) b a)))
+
+(defn find-polyfn-impl [dispatch c]
+  (let [impl #(get dispatch %)]
+    (or (impl c)
+        (and c
+             (or (first (remove nil? (map impl (butlast (super-chain c)))))
+                 (when-let [t (reduce pref
+                                      (filter impl
+                                              (disj (supers c) Object)))]
+                   (impl t))
+                 (impl Object))))))
+
+(defn -cache-polyfn-fn [^AFunction polyfn x]
+  (let [cache (.__methodImplCache polyfn)
+        c (class x)
+        f (find-polyfn-impl @(::dispatch (meta polyfn)) c)]
+    (when-not f
+      (throw (IllegalArgumentException.
+              (str "No implementation of polyfn: " (::var (meta polyfn))
+                   " found for class: " (if (nil? x) "nil" (.getName c))))))
+    (set! (.__methodImplCache polyfn)
+          (expand-method-impl-cache cache c f))
+    f))
+
+(defn- add-impl [^AFunction polyfn type fn]
+  (swap! (::dispatch (meta polyfn)) assoc type fn)
+  (set! (.__methodImplCache polyfn) (MethodImplCache. nil nil)))
 
 (defmacro defpolyfn
-  ([name]
-     `(do (defonce ~name
-            (with-meta
-              (fn [this# & args#]
-                (let [cache# (::cache (meta ~name))
-                      dispatch# @(::dispatch (meta ~name))
-                      c# (class this#)]
-                  (if-let [f# (get @cache# c#)]
-                    (apply f# this# args#)
-                    (if-let [f# (find-polyfn-impl dispatch# c#)]
-                      (do (swap! cache# assoc c# f#)
-                          (apply f# this# args#))
-                      (throw (Exception. (str '~name " is not defined for "
-                                              (pr-str c#))))))))
-              {::cache (atom {})
-               ::dispatch (atom {})}))
-          (var ~name)))
   ([name type params & body]
-     `(do (defpolyfn ~name)
-          (reset! (::cache (meta ~name)) {})
-          (swap! (::dispatch (meta ~name))
-                 assoc ~type (fn [~(with-meta (first params) {:tag type})
-                                  ~@(rest params)]
-                               ~@body))
-          (var ~name))))
+     (let [[target & args] params]
+       `(do (defonce ~name
+              ^{::dispatch (atom {})
+                ::var (var ~name)}
+              (fn polyfn# ~params
+                (let [cache# (.__methodImplCache ^AFunction polyfn#)
+                      f# (.fnFor cache# (clojure.lang.Util/classOf ~target))]
+                  (if f#
+                    (f# ~@params)
+                    ((-cache-polyfn-fn ~name ~target) ~@params)))))
+            (add-impl ~name ~type (fn [~(with-meta target {:tag type})
+                                       ~@args]
+                                    ~@body))
+            (var ~name)))))
